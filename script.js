df24f92c708d658e3b04959780b531573944dc74
Jonatan Sánchez <jona2255@gmail.com>
2021-02-18 18:48:06 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+/* Ejercicio 1 */
+
+const word = "esternocleidomastoideo";
+const wordTransformed = word.charAt(0).toUpperCase() + word.slice(1);
+
+console.log(wordTransformed);
+
+/* Ejercicio 2 */
+/* Parte 1 */
+
+const snakePhrase = "esto_es_una_frase";
+
+const solutionCamel = snakePhrase.replace(/([_][a-z])/g, g => g.toUpperCase().replace("_", ""));
+
+console.log(solutionCamel);
+
+/* Parte 2 */
+
+let solutionSnake = solutionCamel.replace(/([A-Z])/g, g => "_" + g.toLowerCase());
+
+console.log(solutionSnake);
